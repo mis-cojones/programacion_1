4723edbb449bdd83bfba398adf82275d1b7a7a1a
mis-cojones <106718964+mis-cojones@users.noreply.github.com>
2024-09-30 20:36:56 +0000

    finalizacion del ejercicio 5.6

--- ejercicio56.java
+++ ejercicio56.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class ejercicio56 {
     public static void main(String[] args){
         System.out.println("mi primer programa de java en github");
+        Scanner leer = new Scanner(System.in);
+        System.out.println("digite la cantidad de estudiantes: ");
+        int estudiantes= leer.nextInt();
+        double nota = 0.0;
+        int aprobados = 0;
+        int reprobados = 0;
+        for (int i = 1; i <= estudiantes; i++){
+            System.out.println("nota del estudiante" + i + ":");
+            nota = leer.nextDouble(); 
+            if (nota >= 5.0){
+                aprobados++;
+
+            }else{
+                reprobados++;
+            }
+        }
+        System.out.println("aprobados: " + aprobados + "reprobados" + reprobados);
+
+
 
     }
 }
